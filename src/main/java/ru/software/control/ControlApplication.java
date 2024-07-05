d5c2a03990ae6d8ea13565ccf078ba9d39262935
Василенко Илья <ivvasilen1@mts.ru>
2024-07-05 10:00:00 +0300

    feat: init project

---
+++ src/main/java/ru/software/control/ControlApplication.java
@@ -0,0 +1,13 @@
+package ru.software.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/software/control/ControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.software.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//check context elevation
+	}
+}
